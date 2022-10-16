994fa050aa344e1f1029dbf211ea034993043917
diaze0 <92938629+diaze0@users.noreply.github.com>
2022-10-16 18:53:08 +0100

    Create business=promotionBLL.php
    eplincation du code

---
+++ business=promotionBLL.php
@@ -0,0 +1,10 @@
+ Le code est une classe appelée PromotionBLL.
+ Il a quatre méthodes : getAllPromotions(), addPromotion(), getPromotionById() et updatePromotion().
+ La première méthode est utilisée pour récupérer toutes les promotions de la base de données, tandis que les trois autres sont utilisées pour manipuler les données de promotion.
+ Le code est la classe PromotionBLL.
+ La fonction getAllPromotions() renvoie toutes les promotions de cette société.
+ La fonction addPromotion() ajoute une promotion à la base de données.
+ La fonction getPromotionById() obtient une promotion par son numéro d'identification.
+ La fonction updatePromotion() met à jour une promotion existante avec un nouveau nom ou modifie le nom d'une promotion existante.
+ La fonction deletePromotion() supprime une promotion existante de la base de données.
+ Et enfin, la fonction searchByName() recherche les promotions par leur nom afin de les afficher à l'écran ou de les imprimer
