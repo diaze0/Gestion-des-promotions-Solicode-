8b4841f2d8fc58adb55010cb52ec2d007be23c76
diaze0 <92938629+diaze0@users.noreply.github.com>
2022-10-16 19:13:16 +0100

    explication du code

---
+++ data-access=promotion.php
@@ -0,0 +1,9 @@
+ Le code est une classe appelée Promotion.
+ Il a deux variables privées, id et name.
+ La fonction getId() renvoie la valeur de $this->id qui est définie dans le constructeur sur une valeur entière.
+ La fonction setId($id) définit la valeur de $this->id comme étant tout ce qui lui est transmis en tant qu'argument.
+ La fonction getName() renvoie la valeur de $this->name qui est également définie dans le constructeur sur une valeur entière.
+ Enfin, il existe des fonctions publiques pour obtenir et définir des valeurs définies par leurs noms : getId(), setId(), getName() et setName().
+ Le code crée une classe de promotion et définit la propriété id sur private.
+ La méthode getId() renvoie la valeur de la propriété id et setId() définit la valeur de la propriété id.
+ La méthode getName() renvoie la valeur de la propriété name et setName() définit la valeur de la propriété name.
