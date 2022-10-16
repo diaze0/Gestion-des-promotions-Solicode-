d449f6612bdb0a703189e91932ce468fe8e654c7
diaze0 <92938629+diaze0@users.noreply.github.com>
2022-10-16 19:48:16 +0100

    explication du code

---
+++ presentatio/addPromotion.php
@@ -0,0 +1,5 @@
+Le code nécessite que le fichier promotionBLL.php soit inclus pour que cela fonctionne.
+ Le code crée ensuite un nouvel objet de promotion et définit sa propriété name avec la valeur de $_POST['name'].
+ Ensuite, le code redirige vers index.php s'il n'y a pas de messages soumis sur le formulaire.
+ Le code tente d'ajouter une promotion.
+ Le code ne modifie en rien la base de données.
